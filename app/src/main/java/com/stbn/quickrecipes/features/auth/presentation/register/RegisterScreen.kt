311c7a03ee36cfa5ee216050a10143cfd8962c04
Esteban Terrones <esteban20001118@gmail.com>
2026-04-16 18:29:05 -0500

    fix: remove unused imports

--- app/src/main/java/com/stbn/quickrecipes/features/auth/presentation/register/RegisterScreen.kt
+++ app/src/main/java/com/stbn/quickrecipes/features/auth/presentation/register/RegisterScreen.kt
@@ -1,7 +1,6 @@
 package com.stbn.quickrecipes.features.auth.presentation.register
 
 import android.widget.Toast
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
